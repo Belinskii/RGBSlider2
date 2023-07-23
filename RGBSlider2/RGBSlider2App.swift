3893190a4d9e31546f77cf67c8b6331521049bf5
Belinskii <16belinsky@gmail.com>
2023-07-23 16:47:42 +0300

    Initial Commit

---
+++ RGBSlider2/RGBSlider2App.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSlider2App.swift
+//  RGBSlider2
+//
+//  Created by Марина Иванова on 23.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct RGBSlider2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
